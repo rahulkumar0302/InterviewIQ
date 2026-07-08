be780d0ffe852fb0fcf16ec665c4ede5650fc1fb
rahulkumar0302 <rahulkumar9113381925@gmail.com>
2026-07-08 15:20:23 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://interviewiq-g6ss.onrender.com"
+export const ServerUrl  = "https://interviewiq2.onrender.com"
 
 function App() {
 
